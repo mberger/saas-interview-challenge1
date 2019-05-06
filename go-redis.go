b1e97275224e0e1cceb5959b2d0064b099e4b6d4
Michael Berger <mberger@internetinnovation.com>
2019-05-06 14:07:56 -0400

    changed soem files, moved some to a new repo to continue the work I wanted to do.

--- go-redis.go
+++ go-redis.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/garyburd/redigo/redis"
 )
 
-
 func main() {
-c, err := redis.Dial("tcp", ":6379")
-if err != nil {
-panic(err)
-}
-defer c.Close()
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
 
-//set
-c.Do("SET", "message1", "Hello ForgeRock")
+	//set
+	fmt.Println("Publishing to Redis")
 
-//get
-world, err := redis.String(c.Do("GET", "message1"))
-if err != nil {
-fmt.Println("key not found")
-}
+	c.Do("SET", "message1", "Hello ForgeRock")
 
-fmt.Println(world)
-//ENDINIT OMIT
+	//sleep for a 100 ms
+	fmt.Println("Sleeping for 5 seconds")
+	time.Sleep(5000 * time.Millisecond)
+	fmt.Println("Here we go...")
+	fmt.Println(" ")
+	//get
+	world, err := redis.String(c.Do("GET", "message1"))
+	if err != nil {
+		fmt.Println("key not found")
+	}
+
+	fmt.Println(world)
+	//ENDINIT OMIT
 }
